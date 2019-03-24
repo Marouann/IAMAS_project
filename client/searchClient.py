762d4c12add760814c2eed74101b6019f3711614
ipeter50 <pierre.le-jeune@live.fr>
2019-03-24 13:51:37 +0100

    Adding function to perform action and retrieve answer from server.

--- client/searchClient.py
+++ client/searchClient.py
@@ -34,9 +34,9 @@ class SearchClient:
                     initial = True
 
                     row = 0
-                    line = server_messages.readline().rstrip()                    
+                    line = server_messages.readline().rstrip()
 
-                    
+
                 elif line == "#goal":
                     initial = False
                     goal = True
@@ -44,19 +44,19 @@ class SearchClient:
                     row = 0
                 elif line == "#end":
                     goal = False
-                
+
                 if color:
                     splittedLine = line.split(":")
                     self.colors[splittedLine[0]] = splittedLine[1].split(",")
                 if initial:
                     self.initial_state.walls.append([False for _ in range(len(line))])
                     self.initial_state.boxes.append([None for _ in range(len(line))])
-                    
+
                     for col, char in enumerate(line):
                         if char == '+': self.initial_state.walls[row][col] = True
                         elif char in "0123456789":
                             self.initial_state.agents[char] = { "row": row, "col": col }
-                        elif char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ": 
+                        elif char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
                             self.initial_state.boxes[row][col] = char
                         elif char == ' ':
                             # Free cell.
@@ -65,22 +65,43 @@ class SearchClient:
                             print('Error, read invalid level character: {}'.format(char), file=sys.stderr, flush=True)
                             sys.exit(1)
                     row += 1
-                    
+
                 if goal:
                     self.initial_state.goals.append([None for _ in range(len(line))])
                     for col, char in enumerate(line):
                         if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ": self.initial_state.goals[row][col] = char
-                        
+
                     row += 1
-                
+
                 line = server_messages.readline().rstrip()
-               
-            
+
+
         except Exception as ex:
             print('Error parsing level: {}.'.format(repr(ex)), file=sys.stderr, flush=True)
             sys.exit(1)
 
-        print(self.initial_state.walls, file=sys.stderr, flush=True)
+    '''
+    actionList is a 2D array of actions (size number_action_to_execute * number_of_agents).
+        - a row corresponds to a joint action
+        - each col represents action of a specific agent
+
+    return successive result of the server to actions, same size as input
+    '''
+    def executeAction(self,actionsList):
+        server_answer = []
+        for jointAction in actionsList:
+            actions_string = ""
+            for agent_action in jointAction:
+                actions_string += agent_action
+                actions_string += ";"
+            actions_string = actions_string[:-1] # remove last ';' from the string
+            print(actions_string, flush=True) # send action to server
+
+            # retrieve answer from server and separate answer for specific action
+            # [:-1] is only to remove the '\n' at the end of response
+            server_answer.append(sys.stdin.readline()[:-1].split(";"))
+
+        return server_answer
 
 def main():
     # We first declare our name. The server will receive it and be ready to start with us.
@@ -91,9 +112,13 @@ def main():
 
     # Use stderr to print to console through server.
     print('SearchClient initializing. I am sending this using the error output stream.', file=sys.stderr, flush=True)
-    
+
     # Read level and create the initial state of the problem.
     client = SearchClient(server_messages)
+
+    # test actions execution
+    actions = [['Move(W)','Move(E)'], ['Move(W)','Move(E)'], ['Move(W)','Move(E)'], ['Move(W)','Move(E)']]
+    print(client.executeAction(actions), file=sys.stderr, flush=True)
 
 
 if __name__ == '__main__':
