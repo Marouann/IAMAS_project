5ecd9edc38f09d2cd3598a6d21b4a01c03c2ff28
ipeter50 <pierre.le-jeune@live.fr>
2019-03-26 14:54:16 +0100

    Improve classsystem

--- client/action.py
+++ client/action.py
@@ -3,52 +3,33 @@ class Action:
         variables can be anything when defining the actions, they will be replaced by actual variables
         at applicability check or at execution of the action
     '''
-    
-    def __init__(self, name, preconditions, positive_effects, negative_effects, variables):
-        # Name of the action, string
+
+    def __init__(self, name, preconditions, positive_effects, negative_effects):
         self.name = name
-        # Preconditions, list of Atom
         self.preconditions = preconditions
-        # Positive effects, list of Atom
         self.positive_effects = positive_effects
-        # Negative effects, list of Atom
         self.negative_effects = negative_effects
-        # List of the variables, list of string
-        self.variables = variables
-    
-    # checkPreconditions, return True if the preconditions are met
-    def checkPreconditions(self, state, variables):
+
+
+    def isPracticalInState(self, s, variables):
         practical = True
         i = 0
-        variable_map = self.createVariableMap(variables)
-        while practical and i<len(self.preconditions):
-            actual_atom = self.preconditions[i].replace_variables(variable_map)
-            practical = practical and (actual_atom in state.atoms)
-            i += 1 
+        preconditions = self.preconditions(*variables)
+
+        while practical and i<len(preconditions):
+            actual_atom = preconditions[i]
+            practical = practical and (actual_atom in s.atoms)
+            i += 1
         return practical
-    
-    # execute, modify the state if the preconditions are met
-    def execute(self, state, variables):
-        if self.checkPreconditions(state, variables):
-            variable_map = self.createVariableMap(variables)
 
-            for effect in self.negative_effects:
-                state.removeAtom(effect.replace_variables(variable_map))
+    def execute(self, s, variables):
+        if self.isPracticalInState(s, variables):
 
-            for effect in self.positive_effects:
-                state.addAtom(effect.replace_variables(variable_map))
+            for effect in self.negative_effects(*variables):
+                s.removeAtom(effect)
+
+            for effect in self.positive_effects(*variables):
+                s.addAtom(effect)
         else:
             print("This action is not applicable here.")
             
-    # createVariableMap, return a dict of the variables that have to be replaced
-    def createVariableMap(self, variables):
-        variable_map = {}
-        if len(variables) != len(self.variables):
-            print("Wrong number of variables given. Please check action schema.")
-            return None
-       
-        for i, var in enumerate(self.variables):
-            variable_map[var] = variables[i]
-            
-        return variable_map
-        
